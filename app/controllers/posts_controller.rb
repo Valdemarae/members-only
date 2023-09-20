8b7007913cfec37b7906f88c7464157e392dd11d
Valdemar <valdemar.kobelis@gmail.com>
2023-09-20 16:00:02 +0300

    Permit sending params hash when creating a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,18 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = current_user.posts.build
+    @post = current_user.posts.build(post_params)
 
     if @post.save
       redirect_to root_path
     else
       render new_post_path
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
